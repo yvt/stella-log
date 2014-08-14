840410535e3fabda278fba9aa43819b3b5fb6151
yvt <i@yvt.jp>
2014-08-14 18:15:48 +0900

    [StellaDB] Transaction

--- StellaDB/Database.cs
+++ StellaDB/Database.cs
@@ -4,8 +4,48 @@ namespace Yavit.StellaDB
 {
 	public class Database
 	{
+		Transaction currentTransaction = null;
+		LowLevel.LowLevelDatabase lldb;
+
 		public Database ()
 		{
+			throw new NotImplementedException ();
+		}
+
+		void OpenDatabase()
+		{
+			throw new NotImplementedException ();
+		}
+
+		internal void DoAutoCommit()
+		{
+			if (currentTransaction == null) {
+				Commit ();
+			}
+		}
+
+		internal void Commit()
+		{
+			lldb.Flush ();
+			currentTransaction = null;
+		}
+		internal void Rollback()
+		{
+			// Reopnes database to do rollback.
+			// This assumes WAL journaling is being used.
+			OpenDatabase ();
+
+			// TODO: reset other objects
+			currentTransaction = null;
+		}
+
+		public Transaction BeginTransaction()
+		{
+			if (currentTransaction != null) {
+				currentTransaction.Rollback ();
+			}
+			currentTransaction = new Transaction (this);
+			return currentTransaction;
 		}
 	}
 }
---
+++ StellaDB/Transaction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Yavit.StellaDB
+{
+	public class Transaction: IDisposable
+	{
+		Database database;
+		bool commited = false;
+		bool rollbacked = false;
+
+		internal Transaction (Database database)
+		{
+			this.database = database;
+		}
+
+		void CheckState()
+		{
+			if (commited) {
+				throw new InvalidOperationException ("Transaction is already commited.");
+			}
+			if (rollbacked) {
+				throw new InvalidOperationException ("Transaction is already rollbacked.");
+			}
+		}
+
+		public void Commit ()
+		{
+			CheckState ();
+			database.Commit ();
+			commited = true;
+		}
+
+		public void Rollback ()
+		{
+			CheckState ();
+			database.Rollback ();
+			rollbacked = true;
+		}
+
+		public void Dispose ()
+		{
+			if (!commited && !rollbacked) {
+				Rollback ();
+			}
+		}
+	}
+}
+
