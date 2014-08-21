946831bc7f08ca20c6cd2c500a1fc244aaeaf332
yvt <i@yvt.jp>
2014-08-21 21:32:36 +0900

    [StellaDB] Make assembly security-transparent

--- StellaDB/Properties/AssemblyInfo.cs
+++ StellaDB/Properties/AssemblyInfo.cs
@@ -1,8 +1,12 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Security;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
+
+[assembly: SecurityRules(SecurityRuleSet.Level2)]
+[assembly: SecurityTransparent]
 
 [assembly: AssemblyTitle("StellaDB")]
 [assembly: AssemblyDescription("")]
