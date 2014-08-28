fd6cc32be2600d128dad5e5fef11cfad3d2c7e0e
yvt <i@yvt.jp>
2014-08-28 09:41:29 +0900

    [StellaLogCore] NestedTransaction

--- StellaLogCore/LogBook.cs
+++ StellaLogCore/LogBook.cs
@@ -2,15 +2,20 @@
 
 namespace Yavit.StellaLog.Core
 {
-	public class LogBook
+	public class LogBook: IDisposable
 	{
+		readonly NestedTransactionManager transactions;
+
 		internal readonly StellaDB.Database Database;
 		public readonly LocalConfigManager LocalConfig;
 		public readonly VersionController VersionController;
 
+
 		public LogBook (string path)
 		{
 			Database = StellaDB.Database.OpenFile (path);
+			transactions = new NestedTransactionManager (Database);
+
 			using (var t = Database.BeginTransaction()) {
 				LocalConfig = new LocalConfigManager (this);
 				VersionController = new VersionController (this);
@@ -18,7 +23,17 @@ namespace Yavit.StellaLog.Core
 			}
 		}
 
+		public void Dispose ()
+		{
+			transactions.Dispose ();
+			Database.Dispose ();
+		}
 
+
+		public INestedTransaction BeginTransaction()
+		{
+			return transactions.BeginTransaction ();
+		}
 	}
 }
 
---
+++ StellaLogCore/NestedTransaction.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yavit.StellaLog.Core
+{
+	sealed class NestedTransactionManager: IDisposable
+	{
+		readonly StellaDB.Database database;
+
+		StellaDB.Transaction currentDbTransaction = null;
+		readonly Stack<NestedTransaction> stack = new Stack<NestedTransaction>();
+
+		public NestedTransactionManager(StellaDB.Database database)
+		{
+			this.database = database;
+		}
+
+		public void Dispose ()
+		{
+			while (stack.Count > 0) {
+				stack.Pop ().Rollbacked ();
+			}
+			if (currentDbTransaction != null) {
+				currentDbTransaction.Dispose ();
+			}
+		}
+
+		public INestedTransaction BeginTransaction()
+		{
+			var t = new NestedTransaction (this);
+			stack.Push (t);
+			if (stack.Count == 1) {
+				currentDbTransaction = database.BeginTransaction ();
+			}
+			return t;
+		}
+
+		void Rollback(NestedTransaction t)
+		{
+			if (stack.Count == 0 || stack.Peek() != t) {
+				throw new InvalidOperationException ("Outer transaction must be rolled back before " +
+				"disposing the inner transaction.");
+			}
+
+			// All current transactions must be rolled back
+			currentDbTransaction.Dispose ();
+			currentDbTransaction = null;
+			while (stack.Count > 0) {
+				stack.Pop ().Rollbacked ();
+			}
+		}
+
+		void Commit(NestedTransaction t)
+		{
+			if (stack.Count == 0 || stack.Peek() != t) {
+				throw new InvalidOperationException ("Outer transaction must be rolled back or commited before " +
+					"commiting the inner transaction.");
+			}
+
+			stack.Pop ();
+
+			if (stack.Count == 0) {
+				currentDbTransaction.Commit ();
+				currentDbTransaction.Dispose ();
+				currentDbTransaction = null;
+			}
+		}
+
+		enum State
+		{
+			NotCommited,
+			Commited,
+			Rollbacked
+		}
+
+		sealed class NestedTransaction: INestedTransaction, IDisposable
+		{
+			readonly NestedTransactionManager manager;
+			State state = State.NotCommited;
+
+			public NestedTransaction (NestedTransactionManager manager)
+			{
+				this.manager = manager;
+			}
+
+			public void Rollbacked()
+			{
+				state = State.Rollbacked;
+			}
+
+			public void Commit ()
+			{
+				if (state == State.Commited) {
+					throw new InvalidOperationException ("Transaction is already commited.");
+				}
+				if (state == State.Rollbacked) {
+					throw new InvalidOperationException ("Transaction is already rollbacked. " +
+						"This might be because an inner transaction might have been rollbacked.");
+				}
+				manager.Commit (this);
+				state = State.Commited;
+			}
+
+			public void Dispose ()
+			{
+				if (state == State.NotCommited) {
+					manager.Rollback (this);
+					if (state != State.Rollbacked) {
+						throw new InvalidOperationException ();
+					}
+				}
+			}
+		}
+	}
+	public interface INestedTransaction: IDisposable
+	{
+		void Commit();
+	}
+}
+
