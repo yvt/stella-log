d87dceed1395bbaef846073080ec4bcd78292488
yvt <i@yvt.jp>
2014-09-09 15:57:13 +0900

    [StellaLogCoreTest] Added

---
+++ StellaLogCoreTest/LogBookTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Yavit.StellaLog.Core.Test
+{
+	[TestFixture ()]
+	public class LogBookTest
+	{
+		[Test ()]
+		public void Create ()
+		{
+			using (var tmp1 = new TemporaryFile())
+			using (var tmp2 = new TemporaryFile(tmp1.FileName + ".journallog")){
+				new LogBook (tmp1.FileName);
+			}
+		}
+	}
+}
+
---
+++ StellaLogCoreTest/TemporaryFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Yavit.StellaLog.Core.Test
+{
+	public class TemporaryFile: IDisposable
+	{
+		private string fileName;
+		public TemporaryFile (): this(Path.GetTempFileName())
+		{
+		}
+
+		public TemporaryFile (string fileName)
+		{
+			this.fileName = fileName;
+		}
+
+		public string FileName 
+		{
+			get 
+			{
+				return fileName;
+			}
+		}
+
+		public FileStream Open()
+		{
+			return File.Open (fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite,
+				FileShare.Read | FileShare.Delete);
+		}
+
+		public void Dispose ()
+		{
+			if (fileName != null) {
+				try
+				{
+					File.Delete(fileName);
+				}
+				catch { }
+				fileName = null;
+			}
+		}
+	}
+}
+
